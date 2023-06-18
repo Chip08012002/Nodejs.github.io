9764f1289f85e059acde1da8b922248f9ec9ac1e
Chip08012002 <130863118+Chip08012002@users.noreply.github.com>
2023-06-18 20:07:49 +0700

    Router params

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -8,7 +8,7 @@ let getHomepage = async (req, res) => {
 
 let getDetailPage = async(req, res) => {
     let userID = req.params.id;
-    let user = await pool.execute(`SELECT * FROM user where id = ?`, [userID])
+    let user = await pool.execute(`SELECT * FROM user where id = ?`,[userID])
    
     return res.send(JSON.stringify(user[0]))
 }
